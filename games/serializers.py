fe57cb25beb71daeb03ec442077fc9fcbd526011
subha <subha.py@gmail.com>
2017-09-08 16:57:21 +0530

    serializers_completed, pg-93

--- games/serializers.py
+++ games/serializers.py
@@ -24,3 +24,68 @@ class GameCategorySerializer(serializers.HyperlinkedModelSerializer):
             'games'
         )
 
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    game_category =serializers.SlugRelatedField(
+        queryset=GameCategory.objects.all(),
+        slug_field='name'
+    )
+
+    class Meta:
+        model = Game
+        fields = (
+            'url',
+            'game_category',
+            'name',
+            'released_date',
+            'played'
+        )
+
+
+class ScoreSerializer(serializers.HyperlinkedModelSerializer):
+    game = GameSerializer()
+
+    class Meta:
+        model = PlayerScore
+        fields = [
+            'url',
+            'pk',
+            'score',
+            'score_date',
+            'game',
+        ]
+
+class PlayerSerializer(serializers.HyperlinkedModelSerializer):
+    scores = ScoreSerializer(many=True,read_only=True)
+    gender = serializers.ChoiceField(
+        choices = Player.GENDER_CHOICE
+    )
+    gender_description = serializers.CharField(
+        source='get_gender_display',
+        read_only=True
+    )
+
+    class Meta:
+        model = Player
+        fields = [
+            'url',
+            'name',
+            'gender',
+            'gender_description',
+            'scores',
+        ]
+
+class PlayerScoreSerializer(serializers.ModelSerializer):
+    player = serializers.SlugRelatedField(queryset=Player.objects.all(),
+                                          slug_field='name')
+    game=serializers.SlugRelatedField(queryset=Game.objects.all(),slug_field='name')
+
+    class Meta:
+        model = PlayerScore
+        fields = [
+            'url',
+            'pk',
+            'score',
+            'score_date',
+            'player',
+            'game'
+        ]
